c341e094631f8ca57c3554bf3f27fc1885ce4352
motzel <motzel@o2.pl>
2020-07-12 15:52:12 +0200

    Add getProfile, getVideos and getStream functions to Twitch service

--- src/network/twitch.js
+++ src/network/twitch.js
@@ -4,6 +4,23 @@ import {SCORESABER_URL} from "./scoresaber/consts";
 
 const clientId = 'c60s8xch3rksxz2i2rtuof3mczskzc';
 
+const fetchApi = (url, token) => queueFetchJson(
+    queue.TWITCH,
+    url,
+    {
+        headers: {
+            'Client-ID': clientId,
+            'Authorization': `Bearer ${token}`
+        }
+    }
+);
+
 export const getAuthUrl = (state = '', scopes = '') => `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(SCORESABER_URL + '/')}&response_type=token&scope=${encodeURIComponent(scopes)}&state=${encodeURIComponent(state)}`;
 
 export const validateToken = async accessToken => queueFetchJson(queue.TWITCH, 'https://id.twitch.tv/oauth2/validate', {headers: {'Authorization': 'OAuth ' + accessToken}});
+
+export const getProfileByUsername = async (token, userName) => fetchApi(`https://api.twitch.tv/helix/users?login=${encodeURIComponent(userName)}`, token);
+
+export const getVideos = async (token, userId, type = 'archive') => fetchApi(`https://api.twitch.tv/helix/videos?user_id=${encodeURIComponent(userId)}&type=${encodeURIComponent(type)}`, token)
+
+export const getStreams = async (token, userId) => fetchApi(`https://api.twitch.tv/helix/streams?user_id=${encodeURIComponent(userId)}`, token)
--- src/services/twitch.js
+++ src/services/twitch.js
@@ -1,7 +1,8 @@
 import {getFirstRegexpMatch} from "../utils/js";
 import {SCORESABER_URL} from "../network/scoresaber/consts";
 import {getCacheAndConvertIfNeeded, setCache} from "../store";
-import {getAuthUrl, validateToken} from "../network/twitch";
+import {dateFromString} from "../utils/date";
+import {getAuthUrl, validateToken, getProfileByUsername as apiGetProfileByUsername, getVideos as apiGetVideos, getStreams as apiGetStreams} from "../network/twitch";
 
 const getTwitchTokenFromUrl = () => {
     const hash = getFirstRegexpMatch(/scoresaber.com\/#(.*)$/, window.location.href);
@@ -15,9 +16,44 @@ const getTwitchTokenFromUrl = () => {
     return {accessToken: accessTokenMatch[1], url: stateMatch ? SCORESABER_URL + '/u/' + stateMatch[1] : ''};
 }
 
+const getCurrentToken = async () => {
+    const data = await getCacheAndConvertIfNeeded();
+    if (!data || !data.twitch || !data.twitch.token) return null;
+
+    const expires = dateFromString(data.twitch.token.expires);
+    const expiresIn = expires - (new Date());
+
+    return Object.assign({}, data.twitch.token, {expires, expires_in: expiresIn > 0 ? expiresIn : 0});
+}
+
+const getProfileByUsername = async userName => {
+    const token = (await getCurrentToken());
+    if (!token || !token.expires_in || token.expires_in <= 0) return null;
+
+    const profile = await apiGetProfileByUsername(token.accessToken, userName)
+
+    return profile && profile.data && profile.data.length ? profile.data[0] : null;
+}
+
+const getVideos = async (userId, type = 'archive') => {
+    const token = (await getCurrentToken());
+    if (!token || !token.expires_in || token.expires_in <= 0) return null;
+
+    return apiGetVideos(token.accessToken, userId, type)
+}
+
+const getStreams = async userId => {
+    const token = (await getCurrentToken());
+    if (!token || !token.expires_in || token.expires_in <= 0) return null;
+
+    return apiGetStreams(token.accessToken, userId)
+}
+
 export default {
     getAuthUrl,
-
+    getProfileByUsername,
+    getVideos,
+    getStreams,
     processTokenIfAvailable: async () => {
         const twitchToken = getTwitchTokenFromUrl();
         if (twitchToken) {
@@ -26,13 +62,17 @@ export default {
 
             // validate token
             const tokenValidation = await validateToken(twitchToken.accessToken);
+
+            const expiresIn = tokenValidation.expires_in * 1000;
+
             data.twitch.token = Object.assign(
                 {},
                 data.twitch.token,
                 tokenValidation,
                 {
                     accessToken: twitchToken.accessToken,
-                    expires: (new Date(Date.now() + tokenValidation.expires_in * 1000)).toISOString()
+                    expires: (new Date(Date.now() + expiresIn)).toISOString(),
+                    expires_in: expiresIn
                 }
             );
 
