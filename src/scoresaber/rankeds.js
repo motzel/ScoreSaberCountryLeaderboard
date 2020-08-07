0644c46acaade5e6aa6c605159792b516101e1ac
motzel <motzel@o2.pl>
2020-08-07 21:53:28 +0200

    Add unranked map

--- src/scoresaber/rankeds.js
+++ src/scoresaber/rankeds.js
@@ -3,5 +3,5 @@ import {getCacheAndConvertIfNeeded} from "../store";
 export const getRankedSongs = async _ => (await getCacheAndConvertIfNeeded()).rankedSongs;
 
 // errors in API
-export const UNRANKED = [97223];
+export const UNRANKED = [97223, 20506];
 export const RANKED = [102179, 102180]
