2031eaf8ba57c23e5435d9c33a2410d5000b01f5
motzel <motzel@o2.pl>
2020-08-17 13:47:48 +0200

    Add levels support to logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,8 +1,25 @@
 import monkey from '../../monkey.config';
 
+const LEVELS = {
+    NONE: 0,
+    ERROR: 1,
+    WARN: 2,
+    INFO: 3,
+    DEBUG: 4,
+    TRACE: 5
+}
+
+let currentLevel = LEVELS.INFO;
+
 export default {
-    info: message => console.info(`[${monkey.header.name}]`, message),
-    debug: message => console.debug(`[${monkey.header.name}]`, message),
-    warn: message => console.warn(`[${monkey.header.name}]`, message),
-    error: message => console.error(`[${monkey.header.name}]`, message)
+    trace: message => currentLevel >= LEVELS.TRACE ? console.debug(`[${monkey.header.name}]`, message) : null,
+    debug: message => currentLevel >= LEVELS.DEBUG ? console.debug(`[${monkey.header.name}]`, message) : null,
+    info: message => currentLevel >= LEVELS.INFO ? console.info(`[${monkey.header.name}]`, message) : null,
+    warn: message => currentLevel >= LEVELS.WARN ? console.warn(`[${monkey.header.name}]`, message) : null,
+    error: message => currentLevel >= LEVELS.ERROR ? console.error(`[${monkey.header.name}]`, message) : null,
+    ...LEVELS,
+    level: () => currentLevel,
+    setLevel: level => {
+        currentLevel = Object.values(LEVELS).find(l => l === level) ? level : currentLevel
+    }
 };
